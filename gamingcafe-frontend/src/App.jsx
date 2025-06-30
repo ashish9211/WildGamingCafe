9c59c36c8d0d2640d492328707fbbef5d86bd895
Ashish reddy karra <33412622+ashish9211@users.noreply.github.com>
2025-06-30 00:29:44 -0400

    fix gh pages contd

--- gamingcafe-frontend/src/App.jsx
+++ gamingcafe-frontend/src/App.jsx
@@ -1,7 +1,7 @@
 
 import './App.css';
 import './index.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter , Routes, Route} from 'react-router-dom';
 import Navbar from './components/Pages/Navbar';
 import Contact from './components/Pages/ContactPage';
 import Footer from './components/Pages/Footer';
@@ -19,14 +19,14 @@ function App() {
   return (
     <>
    <BrowserRouter basename="/WildGamingCafe">
-    <Router>
+    
       <Navbar/>
         <Routes>
             
             <Route path="/"   element={<HomePage/>} /> 
             <Route path='/contact' element={<Contact/>}/>
             <Route path='/events' element={<EventsPage/>}/>
-            <Route path='/WildGamingCafe/gaminglounge' element={<GamingLounge/>}/>
+            <Route path='/gaminglounge' element={<GamingLounge/>}/>
             <Route path='/promo' element={<BeachWaveAnimation/>}/>
           {/* <Route path='/promo' element={<TestSlideAnimation/>}/> */}
             <Route path='/gamelibrary' element={<GameLibrary/>}/>
@@ -34,7 +34,7 @@ function App() {
         </Routes>
        
       <Footer/>
-    </Router>
+    
   </BrowserRouter> 
       
       
